04be713cae0eb7ef3fa506f2279e9802633c5a56
Wesley Costa <wesleycosta@QTN-30.quatainvestimentos.com.br>
2023-10-10 10:15:45 -0300

    ADD - Front-end do projeto

---
+++ frontend/public/app.js
@@ -0,0 +1,82 @@
+let bytesAmount = 0;
+
+const API_URL = "http://localhost:3000";
+const ON_UPLOAD_EVENT = "file-uploaded";
+
+const formatBytes = (bytes, decimals = 2) => {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];  
+}
+
+const updateStatus = (size) => {
+    const text = `Pending upload: ${formatBytes(size)} bytes`;
+    document.getElementById('size').innerHTML = text;
+
+}
+
+const showSize = () => {
+    const { files: fileElements } = document.getElementById('file');
+    if (!fileElements.length)
+        return console.log("No files selected")
+
+    const files = Array.from(fileElements);
+    const { size } = files    
+        .reduce((prev, next) => ({ size: prev.size + next.size }), { size: 0 });
+
+    bytesAmount = size;
+    updateStatus(size);
+};
+
+const updateMessage = (message) => {
+    const msg = document.getElementById('msg');
+    msg.innerText = message;
+
+    msg.classList.add('alert','alert-success');
+    setTimeout(() => {
+        msg.classList.remove('alert','alert-success');
+        msg.innerText = '';
+    },3000);
+
+}
+
+
+const showMessage = () =>{
+    const urlParams = new URLSearchParams(window.location.search);
+    const serverMessage = urlParams.get('msg');
+
+    if(!serverMessage) return console.log("No message to show");
+    updateMessage(serverMessage);
+}
+
+const configureForm = (targetUrl) => {
+    const form = document.getElementById('form');
+    form.action = targetUrl;
+}
+
+const onload = () => {
+    showMessage();
+    const ioClient = io.connect(API_URL, {withCredentials: false});
+    ioClient.on("connect", (msg) => {
+        console.log("Connected", ioClient.id)
+        const targetUrl = API_URL + `?socketId=${ioClient.id}`;
+        configureForm(targetUrl);
+    });
+
+    ioClient.on(ON_UPLOAD_EVENT,(bytesReceived) => {
+        console.log('Received',bytesReceived)
+        bytesAmount = bytesAmount - bytesReceived;
+        updateStatus(bytesAmount);
+    })
+
+    updateStatus(0);
+}
+
+window.onload = onload;
+window.showSize = showSize;
